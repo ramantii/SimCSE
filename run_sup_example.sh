f292a9dfe70eb8e5703ac9218afff5be47fc008a
ramantii <110367908+ramantii@users.noreply.github.com>
2024-11-13 15:06:10 +0700

    Update run_sup_example.sh

--- run_sup_example.sh
+++ run_sup_example.sh
@@ -3,7 +3,7 @@
 # In this example, we show how to train SimCSE using multiple GPU cards and PyTorch's distributed data parallel on supervised NLI dataset.
 # Set how many GPUs to use
 
-NUM_GPU=4
+NUM_GPU=1
 
 # Randomly set a port number
 # If you encounter "address already used" error, just run again or manually set an available port id.
@@ -23,14 +23,13 @@ python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT
     --per_device_train_batch_size 128 \
     --learning_rate 5e-5 \
     --max_seq_length 32 \
-    --evaluation_strategy steps \
+    --evaluation_strategy no \
+    --save_strategy no \
     --metric_for_best_model stsb_spearman \
     --load_best_model_at_end \
-    --eval_steps 125 \
     --pooler_type cls \
     --overwrite_output_dir \
     --temp 0.05 \
     --do_train \
-    --do_eval \
     --fp16 \
     "$@"
